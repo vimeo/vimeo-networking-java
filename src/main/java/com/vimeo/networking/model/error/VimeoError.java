1135abbb2eb3719767547f1ff588718bb9fd04b8
Kevin Zetterstrom <kevinz@vimeo.com>
2015-06-04 17:13:27 -0400

    removing dead code

--- src/main/java/com/vimeo/networking/model/error/VimeoError.java
+++ src/main/java/com/vimeo/networking/model/error/VimeoError.java
@@ -31,10 +31,7 @@ public class VimeoError extends RuntimeException implements Serializable {
     private String errorCode;
     @SerializedName("invalid_parameters")
     private JsonObject invalidParameters;
-
-    //    public VimeoError(RetrofitError error) {
-//        setRetrofitError(error);
-//    }
+    
     public VimeoError() {
     }
 
