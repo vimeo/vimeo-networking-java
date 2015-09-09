e59c8af14473b6a207907aae6d01bd57c32c216b
Kevin Zetterstrom <kevinz@vimeo.com>
2015-09-09 14:56:32 -0400

    added initials to comment

--- VIMNetworking/src/main/java/com/vimeo/networking/model/StatsCollection.java
+++ VIMNetworking/src/main/java/com/vimeo/networking/model/StatsCollection.java
@@ -11,5 +11,5 @@ public class StatsCollection implements Serializable {
 
     private static final long serialVersionUID = -348202198117360187L;
     @Nullable
-    public Integer plays; // If this is null, that means the uploader has disabled play count
+    public Integer plays; // If this is null, that means the uploader has disabled play count [KZ] 9/9/15
 }
