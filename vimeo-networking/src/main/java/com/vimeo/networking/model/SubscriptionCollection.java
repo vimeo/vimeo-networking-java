d20c24f37e2c4da53e8fe740246ca495de36dbdc
Kevin Zetterstrom <kevinz@vimeo.com>
2016-12-15 16:44:02 -0500

    add annotations

--- vimeo-networking/src/main/java/com/vimeo/networking/model/SubscriptionCollection.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/SubscriptionCollection.java
@@ -2,6 +2,8 @@ package com.vimeo.networking.model;
 
 import com.vimeo.stag.GsonAdapterKey;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -16,23 +18,29 @@ public class SubscriptionCollection implements Serializable {
 
     private static final long serialVersionUID = -6392190720319669273L;
 
+    @Nullable
     @GsonAdapterKey("uri")
     String mUri;
 
+    @Nullable
     @GsonAdapterKey("modified_time")
     Date mModifiedTime;
 
+    @Nullable
     @GsonAdapterKey("subscriptions")
     Subscriptions mSubscriptions;
 
+    @Nullable
     public String getUri() {
         return mUri;
     }
 
+    @Nullable
     public Date getModifiedTime() {
         return mModifiedTime;
     }
 
+    @Nullable
     public Subscriptions getSubscriptions() {
         return mSubscriptions;
     }
