baa77bec30019b6f51d75fd08799d9a24205ef60
Kevin Zetterstrom <kevinz@vimeo.com>
2017-01-23 09:59:17 -0500

    add toString

--- vimeo-networking/src/main/java/com/vimeo/networking/model/notifications/NotificationType.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/notifications/NotificationType.java
@@ -1,6 +1,7 @@
 package com.vimeo.networking.model.notifications;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * A simple enum describing notification types
@@ -32,6 +33,32 @@ public enum NotificationType {
                 return NOTIFICATION_TYPE_VIDEO_AVAILABLE;
             default:
                 return NOTIFICATION_TYPE_UNKNOWN;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return stringFromNotificationType(this);
+    }
+
+    @Nullable
+    public static String stringFromNotificationType(@NotNull NotificationType type) {
+        switch (type) {
+            case NOTIFICATION_TYPE_COMMENT:
+                return NotificationConstants.NOTIFICATION_COMMENT;
+            case NOTIFICATION_TYPE_CREDIT:
+                return NotificationConstants.NOTIFICATION_CREDIT;
+            case NOTIFICATION_TYPE_FOLLOW:
+                return NotificationConstants.NOTIFICATION_FOLLOW;
+            case NOTIFICATION_TYPE_LIKE:
+                return NotificationConstants.NOTIFICATION_LIKE;
+            case NOTIFICATION_TYPE_REPLY:
+                return NotificationConstants.NOTIFICATION_REPLY;
+            case NOTIFICATION_TYPE_VIDEO_AVAILABLE:
+                return NotificationConstants.NOTIFICATION_VIDEO_AVAILABLE;
+            case NOTIFICATION_TYPE_UNKNOWN:
+            default:
+                return null;
         }
     }
 
