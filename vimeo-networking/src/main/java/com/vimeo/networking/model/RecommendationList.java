47034335554dc6904e8d85cc6531730a58ba99d4
Anthony Restaino <AnthonyR@vimeo.com>
2017-01-31 10:47:41 -0500

    Add UseStag to recommendations and season list

--- vimeo-networking/src/main/java/com/vimeo/networking/model/RecommendationList.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/RecommendationList.java
@@ -1,9 +1,12 @@
 package com.vimeo.networking.model;
 
+import com.vimeo.stag.UseStag;
+
 /**
  * A {@link BaseResponseList} for {@link Recommendation} objects
  * Created by zetterstromk on 8/15/16.
  */
+@UseStag
 public class RecommendationList extends BaseResponseList<Recommendation> {
 
     private static final long serialVersionUID = -1488717279892501485L;
--- vimeo-networking/src/main/java/com/vimeo/networking/model/vod/SeasonList.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/vod/SeasonList.java
@@ -1,11 +1,13 @@
 package com.vimeo.networking.model.vod;
 
 import com.vimeo.networking.model.BaseResponseList;
+import com.vimeo.stag.UseStag;
 
 /**
  * A list of {@link Season} items
  * Created by zetterstromk on 10/4/16.
  */
+@UseStag
 public class SeasonList extends BaseResponseList<Season> {
 
     private static final long serialVersionUID = -2072805722241898821L;
