8ff585c63ad073e6a67788328f41e9913c2ddc17
Mohit Sarveiya <mohit@vimeo.com>
2018-04-13 16:20:52 -0400

    Add users with access connection on video object

--- vimeo-networking/src/main/java/com/vimeo/networking/model/Video.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/Video.java
@@ -471,6 +471,17 @@ public class Video implements Serializable {
         }
         return null;
     }
+
+    @Nullable
+    public Connection getUsersWithAccessConnection() {
+        if (mMetadata != null
+            && mMetadata.mConnections != null
+            && mMetadata.mConnections.getUsersWithAccess() != null) {
+            return mMetadata.mConnections.getUsersWithAccess();
+        }
+        return null;
+    }
+
     // </editor-fold>
 
     // <editor-fold desc="Channel">
