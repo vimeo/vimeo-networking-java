6d0403bca8e52fecef044b93a7c53a7efc856495
Howard Rigberg <howard@vimeo.com>
2018-06-22 10:21:12 -0400

    adding support for producer account

--- vimeo-networking/src/main/java/com/vimeo/networking/model/User.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/User.java
@@ -57,6 +57,7 @@ public class User implements Serializable, Followable {
     private static final String ACCOUNT_LIVE_PRO = "live_pro";
     private static final String ACCOUNT_PRO_UNLIMITED = "pro_unlimited";
     private static final String ACCOUNT_LIVE_PREMIUM = "live_premium";
+    private static final String ACCOUNT_PRODUCER = "producer";
 
     public enum AccountType {
         BASIC,
@@ -67,7 +68,8 @@ public class User implements Serializable, Followable {
         LIVE_BUSINESS,
         LIVE_PRO,
         LIVE_PREMIUM,
-        PRO_UNLIMITED
+        PRO_UNLIMITED,
+        PRODUCER
     }
 
     @SerializedName("uri")
@@ -162,6 +164,8 @@ public class User implements Serializable, Followable {
                 return AccountType.PRO_UNLIMITED;
             case ACCOUNT_LIVE_PREMIUM:
                 return AccountType.LIVE_PREMIUM;
+            case ACCOUNT_PRODUCER:
+                return AccountType.PRODUCER;
             case ACCOUNT_BASIC:
             default:
                 return AccountType.BASIC;
