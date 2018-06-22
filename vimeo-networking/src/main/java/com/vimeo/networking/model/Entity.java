23c3d060642f877b5acfc47486187ec6d8d29c4e
Anthony Restaino <AnthonyR@vimeo.com>
2018-06-22 14:39:36 -0400

    Implementing entity in Category

--- vimeo-networking/src/main/java/com/vimeo/networking/model/Entity.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/Entity.java
@@ -1,6 +1,6 @@
 package com.vimeo.networking.model;
 
-import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Represents data that can be uniquely identified by an {@link Entity#identifier()}.
@@ -12,9 +12,9 @@ public interface Entity {
     /**
      * The identifier, as a {@link String}, that uniquely identifies this entity.
      *
-     * @return a non null {@link String}.
+     * @return a {@link String}, or null if the entity does not have an identifier.
      */
-    @NotNull
+    @Nullable
     String identifier();
 
 }
