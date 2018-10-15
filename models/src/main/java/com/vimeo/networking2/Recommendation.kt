1e731b7b7c7eb1ee67c132c8b22334e21aeed96d
Mohit Sarveiya <mohit@vimeo.com>
2018-10-15 14:45:45 -0400

    Add recommendatino DTOs

---
+++ models/src/main/java/com/vimeo/networking2/Recommendation.kt
@@ -0,0 +1,35 @@
+package com.vimeo.networking2
+
+import com.vimeo.networking2.enums.RecommendationType
+
+/**
+ * Recommendation DTO.
+ */
+data class Recommendation(
+
+    /**
+     * The recommended channel.
+     */
+    val channel: Channel? = null,
+
+    /**
+     * The reason for the recommendation.
+     */
+    val description: String? = null,
+
+    /**
+     * The recommendation's resource key string.
+     */
+    val resourceKey: String? = null,
+
+    /**
+     * Type of recommendation.
+     */
+    val type: RecommendationType? = null,
+
+    /**
+     * The user that is being recommended.
+     */
+    val user: User? = null
+
+)
---
+++ models/src/main/java/com/vimeo/networking2/enums/RecommendationType.kt
@@ -0,0 +1,17 @@
+package com.vimeo.networking2.enums
+
+/**
+ * Type of recommendation.
+ */
+enum class RecommendationType {
+
+    /**
+     * The recommendation is for a channel.
+     */
+    CHANNEL,
+
+    /**
+     * The recommendation is for a user.
+     */
+    USER
+}
