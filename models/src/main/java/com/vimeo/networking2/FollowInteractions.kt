6867cb55546ee0d7258a60d538cd98d44f0a3400
Mohit Sarveiya <mohit@vimeo.com>
2018-10-19 15:20:58 -0400

    Move followable extensions to follow interface

--- models/src/main/java/com/vimeo/networking2/FollowInteractions.kt
+++ models/src/main/java/com/vimeo/networking2/FollowInteractions.kt
@@ -9,16 +9,3 @@ interface FollowableInteractions<T: UpdatableInteraction?> {
 
 }
 
-/**
- * Is the user following the object?
- *
- * @return true if it is, otherwise false.
- */
-fun Followable.isFollowing(): Boolean = metadata?.interactions?.follow?.added == true
-
-/**
- * Can the user follow the object?
- *
- * @return true if it is, otherwise false.
- */
-fun Followable.canFollow(): Boolean = metadata?.interactions?.follow?.uri != null
