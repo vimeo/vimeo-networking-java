bf6cbbf7f3865b75d30c0317b3e37e3bdd169256
Anthony Restaino <AnthonyR@vimeo.com>
2018-11-06 16:38:19 -0500

    update BasicInteraction

--- models/src/main/java/com/vimeo/networking2/BasicInteraction.kt
+++ models/src/main/java/com/vimeo/networking2/BasicInteraction.kt
@@ -1,14 +1,13 @@
 package com.vimeo.networking2
 
 import com.vimeo.networking2.common.Interaction
-import com.vimeo.networking2.enums.ApiOptionsType
 
 /**
  * Interaction with options and uri information.
  */
 data class BasicInteraction(
 
-    override val options: List<ApiOptionsType>? = null,
+    override val options: List<String>? = null,
 
     override val uri: String? = null
 
