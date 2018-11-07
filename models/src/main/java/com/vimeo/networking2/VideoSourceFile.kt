0317b605fea2f7e3b0a8efd3fe7ce690afd53152
Anthony Restaino <AnthonyR@vimeo.com>
2018-11-07 14:14:09 -0500

    Adding missing Json annotations to VideoSourceFile

--- models/src/main/java/com/vimeo/networking2/VideoSourceFile.kt
+++ models/src/main/java/com/vimeo/networking2/VideoSourceFile.kt
@@ -2,6 +2,7 @@
 
 package com.vimeo.networking2
 
+import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.vimeo.networking2.enums.VideoQualityType
 import com.vimeo.networking2.enums.VideoSourceType
@@ -14,63 +15,75 @@ data class VideoSourceFile(
     /**
      * The time in ISO 8601 format when the video file was created.
      */
+    @Json(name = "created_time")
     val createdTime: Date? = null,
 
     /**
      * The time in ISO 8601 format when the video file expires.
      */
+    @Json(name = "expires")
     val expires: Date? = null,
 
     /**
      * The FPS of the video.
      */
+    @Json(name = "fps")
     val fps: Int? = null,
 
     /**
      * The height of the video in pixels.
      */
+    @Json(name = "height")
     val height: Int? = null,
 
     /**
      * The direct link to this video file.
      */
+    @Json(name = "link")
     val link: String? = null,
 
     /**
      * Video logging information.
      */
+    @Json(name = "log")
     val log: VideoLog? = null,
 
     /**
      * The MD5 hash of the video file.
      */
+    @Json(name = "md5")
     val md5: String? = null,
 
     /**
      * The video quality (as determined by height and width).
      * @see VideoSourceFile.qualityType
      */
+    @Json(name = "quality")
     val quality: String? = null,
 
     /**
      * The file size of the video.
      */
-    val siz: Int? = null,
+    @Json(name = "size")
+    val size: Int? = null,
 
     /**
      * The source link for the video file.
      */
+    @Json(name = "source_link")
     val sourceLink: String? = null,
 
     /**
      * The type of the video file.
      * @see VideoSourceFile.sourceType
      */
+    @Json(name = "type")
     val type: String? = null,
 
     /**
      * The width of the video in pixels.
      */
+    @Json(name = "width")
     val width: Int? = null
 
 )
