d07fb126f1c9814dd1cffb3414361a300adcee2a
Mohit Sarveiya <mohit@vimeo.com>
2018-11-09 10:54:20 -0500

    Create internal anntoation

---
+++ models/src/main/java/com/vimeo/networking2/annotations/Internal.kt
@@ -0,0 +1,10 @@
+package com.vimeo.networking2.annotations
+
+/**
+ * Annotation to describe certain models as internal.
+ */
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.PROPERTY)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class Internal
