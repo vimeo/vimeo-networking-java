55dac3ee153a0bba7d15f7e1e029386af811bca7
Mohit Sarveiya <mohit@vimeo.com>
2018-12-11 16:43:17 -0500

    Create authenticator class and update config class

---
+++ auth/src/main/java/com/vimeo/networking2/Authenticator.kt
@@ -0,0 +1,7 @@
+package com.vimeo.networking2
+
+/**
+ * Authentication with email, google, facebook or pincode.
+ * This is a temporary class that will be updated as requests are added.
+ */
+class Authenticator(val config: Config)
--- auth/src/main/java/com/vimeo/networking2/Config.kt
+++ auth/src/main/java/com/vimeo/networking2/Config.kt
@@ -6,19 +6,20 @@ import okhttp3.Interceptor
 /**
  * Configuration for authentication and making requests to the Vimeo API.
  *
- * @param clientId          Your client id provided from https://developer.vimeo.com/apps/.
- * @param clientSecret      Your client secret for authentication provided from https://developer.vimeo.com/apps/.
- * @param scopes            A list of your scopes. See https://developer.vimeo.com/api/authentication#scopes.
- * @param accessToken       If you've only provided an access token, you'll only be able to make requests
- *                          for the given access token's account.
- * @param baseUrl           The base url for all requests to the Vimeo API. This is can be overridden to test against a staging sever.
- * @param timeout           Read and the connection timeout for a request. The default time is 60 seconds.
+ * @param clientId              Your client id provided from https://developer.vimeo.com/apps/.
+ * @param clientSecret          Your client secret for authentication provided from https://developer.vimeo.com/apps/.
+ * @param baseUrl               The base url for all requests to the Vimeo API. This is can be overridden to test against a staging sever.
+ * @param scopes                A list of your scopes. See https://developer.vimeo.com/api/authentication#scopes.
+ * @param certPinningEnabled    Enable certificate pining. It is disabled by default.
+ * @param timeout               Read and the connection timeout for a request. The default time is 60 seconds.
+ * @param interceptors          Interceptors to add to for requests.
  */
-class Config(val clientId: String? = null,
-             val clientSecret: String? = null,
-             val scopes: List<ScopeType> = listOf(ScopeType.PUBLIC),
-             val accessToken: String? = null,
-             val baseUrl: String = "https://api.vimeo.com",
-             val timeout: Int = 60,
-             val networkInterceptors: List<Interceptor> = emptyList(),
-             val customInterceptors: List<Interceptor> = emptyList())
+@Suppress("unused", "MemberVisibilityCanBePrivate")
+class Config @JvmOverloads constructor(
+    val clientId: String,
+    val clientSecret: String,
+    val baseUrl: String = "https://api.vimeo.com",
+    val scopes: List<ScopeType> = listOf(ScopeType.PUBLIC),
+    val certPinningEnabled: Boolean = false,
+    val timeout: Int = 60,
+    val interceptors: List<Interceptor>? = null)
