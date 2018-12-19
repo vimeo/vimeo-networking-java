588d7fa059ad377334adafc3e57964897d387174
Mohit Sarveiya <mohit@vimeo.com>
2018-12-19 11:44:39 -0500

    Fix java docs and create api constants class

--- api/src/main/java/com/vimeo/networking2/ServerConfig.kt
+++ api/src/main/java/com/vimeo/networking2/ServerConfig.kt
@@ -11,15 +11,15 @@ import okhttp3.Interceptor
  * @param baseUrl               The base url for all requests to the Vimeo API. This is can be overridden to test against a staging sever.
  * @param scopes                A list of your scopes. See https://developer.vimeo.com/api/authentication#scopes.
  * @param certPinningEnabled    Enable certificate pining. It is disabled by default.
- * @param timeout               Read and the connection timeout for a request. The default time is 60 seconds.
- * @param interceptors          Interceptors to add to for requests.
+ * @param timeout               Read and the connection timeout for a request. The default time is in seconds with a value of 60.
+ * @param interceptors          Custom [Interceptor]s that can be added for each request.
  */
 @Suppress("unused", "MemberVisibilityCanBePrivate")
-class ServerConfig @JvmOverloads constructor(
+data class ServerConfig @JvmOverloads constructor(
     val clientId: String,
     val clientSecret: String,
-    val baseUrl: String = "https://api.vimeo.com",
+    val baseUrl: String = ApiConstants.BASE_URL,
     val scopes: List<ScopeType> = listOf(ScopeType.PUBLIC),
-    val certPinningEnabled: Boolean = false,
-    val timeout: Int = 60,
+    val certPinningEnabled: Boolean = true,
+    val timeout: Int = ApiConstants.READ_CONNECTION_TIMEOUT_SECONDS,
     val interceptors: List<Interceptor>? = null)
--- auth/src/main/java/com/vimeo/networking2/Authenticator.kt
+++ auth/src/main/java/com/vimeo/networking2/Authenticator.kt
@@ -3,5 +3,9 @@ package com.vimeo.networking2
 /**
  * Authentication with email, google, facebook or pincode.
  * This is a temporary class that will be updated as requests are added.
+ *
+ * @param serverConfig All the server configuration (client id and secret, custom interceptors,
+ *                     read timeouts, base url etc...) that can be set for authentication and
+ *                     making requests.
  */
-class Authenticator(val serverConfig: ServerConfig)
+class Authenticator(private val serverConfig: ServerConfig)
