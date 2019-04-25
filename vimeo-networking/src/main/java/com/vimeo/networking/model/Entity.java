2a0c4194e5f89e5d016ea186700a3aa49fa8fc32
Mohit Sarveiya <mohit@vimeo.com>
2019-04-25 11:30:19 -0400

    Update old Entity inteface to conform to new Entity interface

--- vimeo-networking/src/main/java/com/vimeo/networking/model/Entity.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/Entity.java
@@ -1,20 +1,8 @@
 package com.vimeo.networking.model;
 
-import org.jetbrains.annotations.Nullable;
-
 /**
  * Represents data that can be uniquely identified by an {@link Entity#getIdentifier()}.
  * <p>
  * Implementors of this interface must provide a unique identifier.
  */
-public interface Entity {
-
-    /**
-     * The identifier, as a {@link String}, that uniquely identifies this entity.
-     *
-     * @return a {@link String}, or null if the entity does not have an identifier.
-     */
-    @Nullable
-    String getIdentifier();
-
-}
+public interface Entity extends com.vimeo.networking2.common.Entity { }
