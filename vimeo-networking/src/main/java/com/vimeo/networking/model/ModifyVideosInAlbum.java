cbdbe131cb00c6098c2aee0c4d2551795bfb4934
Howard Rigberg <howard@vimeo.com>
2019-05-30 15:35:20 -0400

    adding named wrappers

--- vimeo-networking/src/main/java/com/vimeo/networking/model/ModifyVideosInAlbum.java
+++ vimeo-networking/src/main/java/com/vimeo/networking/model/ModifyVideosInAlbum.java
@@ -15,21 +15,45 @@ public class ModifyVideosInAlbum implements Serializable {
 
     private static final long serialVersionUID = -3094719083671086785L;
 
+    private class NamedWrapperForAdd implements Serializable{
+
+        private static final long serialVersionUID = 8713902512465812810L;
+
+        NamedWrapperForAdd(List<AddVideoToAlbum> addVideoList) {
+            mAddVideosToAlbums = new ArrayList<>(addVideoList);
+        }
+
+        @SerializedName("video")
+        public List<AddVideoToAlbum> mAddVideosToAlbums;
+    }
+
+    private class NamedWrapperForRemove implements Serializable{
+
+        private static final long serialVersionUID = -8122735684596463036L;
+
+        NamedWrapperForRemove(List<RemoveVideoFromAlbum> removeVideoList) {
+            mRemoveVideosFromAlbums = new ArrayList<>(removeVideoList);
+        }
+
+        @SerializedName("video")
+        public List<RemoveVideoFromAlbum> mRemoveVideosFromAlbums;
+    }
+
     public ModifyVideosInAlbum(@Nullable List<RemoveVideoFromAlbum> removeVideoList,
                                @Nullable List<AddVideoToAlbum> addVideoList) {
         if (removeVideoList != null) {
-            mRemoveVideoList = new ArrayList<>(removeVideoList);
+            mRemoveVideoList = new NamedWrapperForRemove(removeVideoList);
         }
         if (addVideoList != null) {
-            mAddVideoList = new ArrayList<>(addVideoList);
+            mAddVideoList = new NamedWrapperForAdd(addVideoList);
         }
     }
 
     @Nullable
     @SerializedName("remove")
-    public List<RemoveVideoFromAlbum> mRemoveVideoList;
+    public NamedWrapperForRemove mRemoveVideoList;
 
     @Nullable
     @SerializedName("set")
-    public List<AddVideoToAlbum> mAddVideoList;
+    public NamedWrapperForAdd mAddVideoList;
 }
