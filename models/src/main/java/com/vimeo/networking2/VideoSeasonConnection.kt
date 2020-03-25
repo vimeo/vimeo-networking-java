04a0499242ab2baef739544f44d8107dd57b496f
Anthony Restaino <AnthonyR@vimeo.com>
2020-03-25 14:33:31 -0400

    Adding missing VideoSeasonConnection

---
+++ models/src/main/java/com/vimeo/networking2/VideoSeasonConnection.kt
@@ -0,0 +1,29 @@
+package com.vimeo.networking2
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+/**
+ * Information about the season the video is associated with.
+ */
+@JsonClass(generateAdapter = true)
+data class VideoSeasonConnection(
+
+    /**
+     * An array of HTTP methods permitted on this URI.
+     */
+    @Json(name = "options")
+    val options: List<String>? = null,
+
+    /**
+     * The API URI that resolves to the connection data.
+     */
+    @Json(name = "uri")
+    val uri: String? = null,
+
+    /**
+     * The name of the season.
+     */
+    @Json(name = "name")
+    val name: String? = null
+)
