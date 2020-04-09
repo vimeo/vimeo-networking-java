58e52c73cd2a3aef637383fecf3feaac34c46a77
Anthony Restaino <AnthonyR@vimeo.com>
2020-04-09 14:42:51 -0400

    Fixing bug where occassionally the accessToken is null and it was crashing

--- vimeo-networking/src/main/java/com/vimeo/networking/utils/VimeoAccountFactory.kt
+++ vimeo-networking/src/main/java/com/vimeo/networking/utils/VimeoAccountFactory.kt
@@ -7,6 +7,6 @@ import com.vimeo.networking2.VimeoAccount
 /**
  * Create a [VimeoAccount] with only the provided [accessToken] set.
  */
-fun createVimeoAccountWithToken(accessToken: String): VimeoAccount = VimeoAccount(
+fun createVimeoAccountWithToken(accessToken: String?): VimeoAccount = VimeoAccount(
     accessToken = accessToken
 )
