8479c345da24f69e2b4e2f43a5de57b205aeadbf
Anthony Restaino <AnthonyR@vimeo.com>
2020-06-25 14:57:09 -0400

    Make grant type and scope type implement StringValue for consistency

--- api-core/src/main/java/com/vimeo/networking2/GrantType.kt
+++ api-core/src/main/java/com/vimeo/networking2/GrantType.kt
@@ -1,9 +1,11 @@
 package com.vimeo.networking2
 
+import com.vimeo.networking2.enums.StringValue
+
 /**
  * Grant types.
  */
-enum class GrantType(val value: String) {
+enum class GrantType(override val value: String) : StringValue {
     CLIENT_CREDENTIALS("client_credentials"),
     AUTHORIZATION_CODE("authorization_code"),
     PASSWORD("password"),
--- api-core/src/main/java/com/vimeo/networking2/ScopeType.kt
+++ api-core/src/main/java/com/vimeo/networking2/ScopeType.kt
@@ -1,11 +1,13 @@
 package com.vimeo.networking2
 
+import com.vimeo.networking2.enums.StringValue
+
 /**
  * All scopes that can be passed to the API. They determine what information you
  * could get and your permissions in the Vimeo API. Here is more info on all the
  * supported scopes @see https://developer.vimeo.com/api/authentication#scopes.
  */
-enum class ScopeType(val value: String) {
+enum class ScopeType(override val value: String) : StringValue {
     PRIVATE("private"),
     PUBLIC("public"),
     PURCHASED("purchased"),
