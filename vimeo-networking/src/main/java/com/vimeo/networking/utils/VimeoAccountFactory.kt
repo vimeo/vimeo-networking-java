a02c9bb83bb79abe7a68d78f66656c527f1c8d99
Anthony Restaino <AnthonyR@vimeo.com>
2020-07-08 10:53:49 -0400

    Adding todo

--- vimeo-networking/src/main/java/com/vimeo/networking/utils/VimeoAccountFactory.kt
+++ vimeo-networking/src/main/java/com/vimeo/networking/utils/VimeoAccountFactory.kt
@@ -9,6 +9,7 @@ import com.vimeo.networking2.VimeoAccount
  * Create a [VimeoAccount] with only the provided [accessToken] set.
  */
 @Suppress("UnsafeCallOnNullableType")
+// TODO: Remove usage of this function
 fun createVimeoAccountWithToken(accessToken: String?): VimeoAccount = VimeoAccount(
     accessToken = accessToken!!,
     user = User()
