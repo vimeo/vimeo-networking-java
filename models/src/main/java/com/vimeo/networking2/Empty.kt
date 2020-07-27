01f8c99ff7ae40c90b7d842509bab226c4e7e690
Howard Rigberg <howard@vimeo.com>
2020-07-27 16:09:05 -0400

    Update Empty.kt

--- models/src/main/java/com/vimeo/networking2/Empty.kt
+++ models/src/main/java/com/vimeo/networking2/Empty.kt
@@ -1,9 +1,12 @@
 package com.vimeo.networking2
 
+import com.squareup.moshi.JsonClass
+
 /**
  * This class is here for when you are declaring a generic type in Kotlin that also needs
  * to be used by Java and the type generator. We can't use Nothing because Java doesn't know
  * about this type and we can't use Void because it is not parcelable and breaks the type
  * generator.
  */
+@JsonClass(generateAdapter = true)
 class Empty
