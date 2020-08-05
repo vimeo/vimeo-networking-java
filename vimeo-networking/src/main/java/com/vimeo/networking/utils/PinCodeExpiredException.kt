4ef1f73806a811295c0d49aa8bbd8d57d6fb0349
Anthony Restaino <AnthonyR@vimeo.com>
2020-08-05 13:09:28 -0400

    Removing old networking code

--- vimeo-networking/src/main/java/com/vimeo/networking/utils/PinCodeExpiredException.kt
+++
@@ -1,8 +0,0 @@
-package com.vimeo.networking.utils
-
-import java.lang.IllegalStateException
-
-/**
- * An exception indicating that a pin code has expired.
- */
-class PinCodeExpiredException(override val message: String) : IllegalStateException(message)
