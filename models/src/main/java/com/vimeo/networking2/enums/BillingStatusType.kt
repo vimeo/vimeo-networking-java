e69a4df879a60584eb3dfaabbb852698594e9362
WhosNickDoglio <nicholasdoglio@gmail.com>
2020-10-27 13:29:10 -0400

    Change expired to pending and updated comments

--- models/src/main/java/com/vimeo/networking2/enums/BillingStatusType.kt
+++ models/src/main/java/com/vimeo/networking2/enums/BillingStatusType.kt
@@ -6,27 +6,30 @@ package com.vimeo.networking2.enums
 enum class BillingStatusType(override val value: String?) : StringValue {
 
     /**
-     * The user's billing information is active.
+     * The user's billing information is active and will auto-renew.
      */
     ACTIVE("active"),
 
     /**
-     * The user's billing information has been canceled.
+     * The user's billing information has been canceled, but the subscription could still be active depending
+     * on if the expiration date has passed.
      */
     CANCELED("canceled"),
 
     /**
-     * The user's billing information has expired.
+     * The user's billing information is in a pending state and is likely to change.
      */
-    EXPIRED("expired"),
+    PENDING("pending"),
 
     /**
-     * The user's billing information is in a grace period before it's put on hold.
+     * The user's billing information is in a grace period before it's put on hold while the auto-renew
+     * charge is retried.
      */
     GRACE_PERIOD("grace_period"),
 
     /**
-     * The user's billing information is currently on hold.
+     * The user's billing information is currently on hold due to a payment failure and the grace period expiring.
+     * (Google Play only)
      */
     ON_HOLD("on_hold"),
 
