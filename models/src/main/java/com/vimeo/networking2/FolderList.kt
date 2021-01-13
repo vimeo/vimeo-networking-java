386342a681d3609a4e81c8d7c421f8d55db3fa24
Anthony Restaino <AnthonyR@vimeo.com>
2021-01-13 16:44:43 -0500

    Create FolderList DTO

---
+++ models/src/main/java/com/vimeo/networking2/FolderList.kt
@@ -0,0 +1,29 @@
+package com.vimeo.networking2
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import com.vimeo.networking2.common.Pageable
+
+
+/**
+ * List of [Folder]s to show in a user's feed.
+ */
+@JsonClass(generateAdapter = true)
+class FolderList(
+
+    @Json(name = "total")
+    override val total: Int? = null,
+
+    @Json(name = "page")
+    override val page: Int? = null,
+
+    @Json(name = "per_page")
+    override val perPage: Int? = null,
+
+    @Json(name = "paging")
+    override val paging: Paging? = null,
+
+    @Json(name = "data")
+    override val data: List<Folder>? = null
+
+) : Pageable<Folder>
