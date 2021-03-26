e6f647d342a45500176c57c504c9ac26d48b4172
WhosNickDoglio <nicholasdoglio@gmail.com>
2021-03-26 09:13:40 -0400

    Changing TeamMembershipList to inherit from Page instead of Pageable

--- models/src/main/java/com/vimeo/networking2/TeamMembershipList.kt
+++ models/src/main/java/com/vimeo/networking2/TeamMembershipList.kt
@@ -2,23 +2,17 @@ package com.vimeo.networking2
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
-import com.vimeo.networking2.common.Pageable
+import com.vimeo.networking2.common.Page
 
 /**
- * List of [TeamMemberships][TeamMembership] that can be paged.
+ * List of [TeamMemberships][TeamMembership] that cannot be paged.
  */
 @JsonClass(generateAdapter = true)
 data class TeamMembershipList(
     @Json(name = "total")
     override val total: Int? = null,
-    @Json(name = "page")
-    override val page: Int? = null,
-    @Json(name = "per_page")
-    override val perPage: Int? = null,
-    @Json(name = "paging")
-    override val paging: Paging? = null,
     @Json(name = "data")
     override val data: List<TeamMembership>? = null,
     @Json(name = "filtered_total")
     override val filteredTotal: Int? = null
-) : Pageable<TeamMembership>
+) : Page<TeamMembership>
