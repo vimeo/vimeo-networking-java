5833ae6677d98a6240681ce6f4f8ea335bc24d85
Anthony Restaino <anthonyr@vimeo.com>
2021-04-06 13:50:23 -0400

    Allow non ascii characters as a user agent

--- api-core/src/main/java/com/vimeo/networking2/internal/interceptor/UserAgentHeaderInterceptor.kt
+++ api-core/src/main/java/com/vimeo/networking2/internal/interceptor/UserAgentHeaderInterceptor.kt
@@ -32,8 +32,9 @@ import okhttp3.Response
  * @param userAgent The user agent that should be sent with every request.
  */
 class UserAgentHeaderInterceptor(private val userAgent: String) : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response =
-        chain.proceed(chain.request().newBuilder().header(HEADER_USER_AGENT, userAgent).build())
+    override fun intercept(chain: Interceptor.Chain): Response = chain.proceed(chain.request().run {
+        newBuilder().headers(headers().newBuilder().addUnsafeNonAscii(HEADER_USER_AGENT, userAgent).build()).build()
+    })
 
     companion object {
         private const val HEADER_USER_AGENT = "User-Agent"
