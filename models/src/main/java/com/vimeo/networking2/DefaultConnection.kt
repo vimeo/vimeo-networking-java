4a65f93cfc134248739cf9bf409ad10615a70baf
Anthony Restaino <anthonyr@vimeo.com>
2021-04-26 15:02:25 -0400

    Making connection a data class

--- models/src/main/java/com/vimeo/networking2/DefaultConnection.kt
+++ models/src/main/java/com/vimeo/networking2/DefaultConnection.kt
@@ -8,7 +8,7 @@ import com.vimeo.networking2.common.Connection
  * The default implementation of a connection that does not add any other properties.
  */
 @JsonClass(generateAdapter = true)
-class DefaultConnection(
+data class DefaultConnection(
     @Json(name = "options")
     override val options: List<String>? = null,
 
