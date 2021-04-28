82fff9c843c3f1a138b9ddf07fd9390a4a51b1f0
Anthony Restaino <anthonyr@vimeo.com>
2021-04-28 15:43:29 -0400

    Making sure the plugin runs on iOS and Android

--- model-generator/plugin/src/main/java/com/vimeo/modelgenerator/GenerateModelsPlugin.kt
+++ model-generator/plugin/src/main/java/com/vimeo/modelgenerator/GenerateModelsPlugin.kt
@@ -11,32 +11,27 @@ class GenerateModelsPlugin : Plugin<Project> {
         val extension =
             project.extensions.create(EXTENSION_NAME, GenerateModelsExtension::class.java, project)
 
-        // Android projects can also have the JVM plugin applied, so we ensure to only register the task once.
-        if (project.plugins.findPlugin(KOTLIN_ANDROID) != null) {
-            // Sets up task on Android based projects.
-            project.plugins.withId(KOTLIN_ANDROID) {
-                registerTask(project, extension)
-                project.tasks.findByName(PRE_BUILD)?.dependsOn(GENERATE_MODELS)
-            }
-        } else {
-            // Sets up tasks on JVM based projects.
-            project.plugins.withId(KOTLIN_JVM) {
-                registerTask(project, extension)
+        project.pluginManager.withPlugin(KOTLIN_ANDROID) {
+            registerTask(project, extension)
+            project.tasks.findByName(PRE_BUILD)?.dependsOn(GENERATE_MODELS)
+        }
 
-                // kaptGenerateStubsKotlin is used as the set up task instead of build
-                // because kapt code generation happens before build is called and
-                // we need to generate the models prior to kapt so the Moshi adapters
-                // can also be generated.
-                val kaptTask: Task? = project.tasks.findByName(KAPT_GENERATE_STUBS)
+        project.pluginManager.withPlugin(KOTLIN_JVM) {
+            registerTask(project, extension)
 
-                // When generating to a module that doesn't use kapt this task will be null
-                // in that case compileKotlin is the earliest task run when the module is being
-                // built so we can attach generateModels to that instead.
-                if (kaptTask != null) {
-                    kaptTask.dependsOn(GENERATE_MODELS)
-                } else {
-                    project.tasks.findByName(COMPILE_KOTLIN)?.dependsOn(GENERATE_MODELS)
-                }
+            // kaptGenerateStubsKotlin is used as the set up task instead of build
+            // because kapt code generation happens before build is called and
+            // we need to generate the models prior to kapt so the Moshi adapters
+            // can also be generated.
+            val kaptTask: Task? = project.tasks.findByName(KAPT_GENERATE_STUBS)
+
+            // When generating to a module that doesn't use kapt this task will be null
+            // in that case compileKotlin is the earliest task run when the module is being
+            // built so we can attach generateModels to that instead.
+            if (kaptTask != null) {
+                kaptTask.dependsOn(GENERATE_MODELS)
+            } else {
+                project.tasks.findByName(COMPILE_KOTLIN)?.dependsOn(GENERATE_MODELS)
             }
         }
     }
