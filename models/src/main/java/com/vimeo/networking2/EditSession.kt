8c50af493c8602b6f2e627126850bc0606de26e9
Alexander Baltser <alexander.baltser@vimeo.com>
2021-06-17 15:45:08 +0300

    VCANDROID-1140 Fix docs for class EditSession

--- models/src/main/java/com/vimeo/networking2/EditSession.kt
+++ models/src/main/java/com/vimeo/networking2/EditSession.kt
@@ -5,42 +5,32 @@ import com.squareup.moshi.JsonClass
 
 /**
  * Information about the Vimeo Create session of a video.
+ * 
+ * @param isMusicLicensed Whether the video has licensed music.
+ * @param isMaxResolution Whether the current version of clip is of max resolution.
+ * @param vsid Video session id.
+ * @param resultVideoHash The result video hash.
+ * @param hasWatermark Whether the video has watermark.
+ * @param isRated Whether the video is rated.
  */
 @JsonClass(generateAdapter = true)
 data class EditSession(
-    /**
-     * Whether the video has licensed music.
-     */
+
     @Json(name = "is_music_licensed")
     val isMusicLicensed: Boolean? = null,
 
-    /**
-     * Whether the current version of clip is of max resolution.
-     */
     @Json(name = "is_max_resolution")
     val isMaxResolution: Boolean? = null,
 
-    /**
-     * Video session id.
-     */
     @Json(name = "vsid")
     val vsid: Int? = null,
 
-    /**
-     * The result video hash.
-     */
     @Json(name = "result_video_hash")
     val resultVideoHash: String? = null,
 
-    /**
-     * Whether the video has watermark.
-     */
     @Json(name = "has_watermark")
     val hasWatermark: Boolean? = null,
 
-    /**
-     * Whether the video is rated.
-     */
     @Json(name = "is_rated")
     val isRated: Boolean? = null,
 )
