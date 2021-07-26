c12cc50828eb1429abc6701b375ba26b062f018e
Anthony Restaino <anthonyr@vimeo.com>
2021-07-26 11:58:59 -0400

    Improving error docs and removing unused property

--- models/src/main/java/com/vimeo/networking2/ApiError.kt
+++ models/src/main/java/com/vimeo/networking2/ApiError.kt
@@ -11,11 +11,11 @@ import com.vimeo.networking2.enums.asEnum
  * This class represents an error response from the Vimeo API. It holds useful getters to
  * understand why your request might have failed.
  *
- * @param developerMessage Developer friendly error message.
- * @param userMessage User friendly error message, can be used for displaying messages to the user.
- * @param errorMessage User friendly error message.
+ * @param developerMessage The technical reason for the error.
+ * @param errorMessage The error message, can be used for displaying messages to the user if [developerMessage] is also
+ * provided. If only [errorMessage] is provided then it may not be properly localized.
  * @param errorCode The error code. See [ApiError.errorCodeType].
- * @param invalidParameters Information on invalid parameters send in the request.
+ * @param invalidParameters Information on any invalid parameters that were sent in the request.
  * @param link A link to more information about the error.
  */
 @JsonClass(generateAdapter = true)
@@ -23,9 +23,6 @@ data class ApiError(
 
     @Json(name = "developer_message")
     val developerMessage: String? = null,
-
-    @Json(name = "user_message")
-    val userMessage: String? = null,
 
     @Json(name = "error")
     val errorMessage: String? = null,
