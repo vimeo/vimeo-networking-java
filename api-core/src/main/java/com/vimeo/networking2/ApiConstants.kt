ecccd51327e54b2a44be7e901ce43f750afdc325
Howard Rigberg <howard@vimeo.com>
2021-08-03 17:52:20 -0400

    preparing 2.0.1 release

--- api-core/src/main/java/com/vimeo/networking2/ApiConstants.kt
+++ api-core/src/main/java/com/vimeo/networking2/ApiConstants.kt
@@ -34,7 +34,7 @@ object ApiConstants {
 
     const val SSL_PUBLIC_KEY = "sha256/5kJvNEMw0KjrCAu7eXY5HZdvyCS13BbA0VJG1RSP91w="
 
-    const val SDK_VERSION = "2.0.0"
+    const val SDK_VERSION = "2.0.1"
 
     const val NONE = -1
 
