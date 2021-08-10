5151ae5a3f04bb0f4da08d62070eafa53ee1e57d
Ievgenii Tkachenko <ievgenii.tkachenko@vimeo.com>
2021-08-10 13:52:43 +0300

    VCANDROID-1320 Add edit_session.min_tier_for_movie field

--- models/src/main/java/com/vimeo/networking2/EditSession.kt
+++ models/src/main/java/com/vimeo/networking2/EditSession.kt
@@ -12,6 +12,7 @@ import com.squareup.moshi.JsonClass
  * @param resultVideoHash The result video hash.
  * @param hasWatermark Whether the video has watermark.
  * @param isRated Whether the video is rated.
+ * @param minTierForMovie Whether video requires paid plan. .
  */
 @JsonClass(generateAdapter = true)
 data class EditSession(
@@ -33,4 +34,7 @@ data class EditSession(
 
     @Json(name = "is_rated")
     val isRated: Boolean? = null,
+
+    @Json(name = "min_tier_for_movie")
+    val minTierForMovie: String? = null,
 )
