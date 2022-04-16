5051f1ca005fd01c8fbe65ec911a56595a601ada
ed.hicks@vimeo.com <ed.hicks@vimeo.com>
2022-04-15 23:25:02 -0400

    json annotations

--- models/src/main/java/com/vimeo/networking2/PermissionPolicyList.kt
+++ models/src/main/java/com/vimeo/networking2/PermissionPolicyList.kt
@@ -1,12 +1,19 @@
 package com.vimeo.networking2
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
 /**
  * Represents a list of [PermissionPolicy]
  *
  * @param total the number of policies in the list returned in [data]
  * @param data the actual list of [PermissionPolicy]s
  */
+@JsonClass(generateAdapter = true)
 class PermissionPolicyList(
+    @Json(name = "total")
     val total: Int? = null,
-    val data: List<PermissionPolicy>
+
+    @Json(name = "data")
+    val data: List<PermissionPolicy>? = null
 )
