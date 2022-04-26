0491590da07bea69328cca93925a80fbf1c6ed66
Anthony Restaino <anthonyr@vimeo.com>
2022-04-26 10:51:23 -0400

    Rename edit interaction to addRemoveVideos to be more clear about its purpose

--- models/src/main/java/com/vimeo/networking2/FolderInteractions.kt
+++ models/src/main/java/com/vimeo/networking2/FolderInteractions.kt
@@ -6,13 +6,16 @@ import com.squareup.moshi.JsonClass
 /**
  * The interactions for a folder.
  *
+ * @param addRemoveVideos The interaction used to determine if the user can add to or remove videos from the folder.
  * @param addSubfolder The interaction used to add a subfolder as well as determine capability for adding subfolders.
  * @param deleteVideo The interaction that shows whether the user can delete videos from the folder.
- * @param edit The interaction that shows whether the user can edit the folder's settings.
  * @param invite The interaction that shows whether the user can invite other users to manage the folder.
  */
 @JsonClass(generateAdapter = true)
 data class FolderInteractions(
+
+    @Json(name = "edit")
+    val addRemoveVideos: BasicInteraction? = null,
 
     @Json(name = "add_subfolder")
     val addSubfolder: AddSubfolderInteraction? = null,
@@ -20,8 +23,6 @@ data class FolderInteractions(
     @Json(name = "delete_video")
     val deleteVideo: BasicInteraction? = null,
 
-    @Json(name = "edit")
-    val edit: BasicInteraction? = null,
 
     @Json(name = "invite")
     val invite: BasicInteraction? = null,
