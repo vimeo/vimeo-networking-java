85eb29daa9692ea3f578f631eb7a5a5240440819
ed.hicks@vimeo.com <ed.hicks@vimeo.com>
2022-06-07 23:36:45 -0400

    Update PermissionPolicy with missing fields.

--- models/src/main/java/com/vimeo/networking2/PermissionPolicy.kt
+++ models/src/main/java/com/vimeo/networking2/PermissionPolicy.kt
@@ -2,15 +2,18 @@ package com.vimeo.networking2
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
+import com.vimeo.networking2.common.Entity
 import java.util.Date
 
 /**
  * A permission policy which represents a collection or permutation of certain [PermissionActions].
  *
- * @property uri The uri for the permission policy
- * @property name The displayable name for the permission policy
- * @property createdOn The date the policy was created
- * @property modifiedOn The date the policy was last modified on
+ * @property uri The uri for the permission policy.
+ * @property name The displayable name for the permission policy.
+ * @param displayName The name of the permission level, translated and ready for client display.
+ * @param displayDescription The description of the permission level, for client display.
+ * @property createdOn The date the policy was created.
+ * @property modifiedOn The date the policy was last modified on.
  * @property permissionActions The actions the user can take.
  */
 @JsonClass(generateAdapter = true)
@@ -22,6 +25,12 @@ data class PermissionPolicy(
     @Json(name = "name")
     val name: String? = null,
 
+    @Json(name = "display_name")
+    val displayName: String? = null,
+
+    @Json(name = "display_description")
+    val displayDescription: String? = null,
+
     @Json(name = "created_on")
     val createdOn: Date? = null,
 
@@ -30,4 +39,7 @@ data class PermissionPolicy(
 
     @Json(name = "permission_actions")
     val permissionActions: PermissionActions? = null
-)
+) :Entity {
+    override val identifier: String?
+        get() = uri
+}
