1613399663893824bd292973ea1c940665f4be98
Yevhen Stepiuk <yevhen.stepiuk@vimeo.com>
2022-12-09 17:29:11 +0200

    VIMAND-160 Fix format in FeaturedContent.kt

--- models/src/main/java/com/vimeo/networking2/FeaturedContent.kt
+++ models/src/main/java/com/vimeo/networking2/FeaturedContent.kt
@@ -3,16 +3,15 @@ package com.vimeo.networking2
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
-
 /**
  * Featured content data.
- * 
+ *
  * @param video Featured video.
  */
 @JsonClass(generateAdapter = true)
 data class FeaturedContent(
-    
+
     @Json(name = "video")
     val video: Video?,
-    
+
 )
