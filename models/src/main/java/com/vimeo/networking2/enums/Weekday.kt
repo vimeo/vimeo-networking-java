8008975876d5744f1ab80efbcfb196305f8181a4
Yevhen Stepiuk <yevhen.stepiuk@vimeo.com>
2023-01-31 18:21:40 +0200

    CXAN-71 Add calendarWeekday property to Weekday

--- models/src/main/java/com/vimeo/networking2/enums/Weekday.kt
+++ models/src/main/java/com/vimeo/networking2/enums/Weekday.kt
@@ -1,16 +1,21 @@
 package com.vimeo.networking2.enums
 
+import java.util.Calendar
+
 /**
  * Enum class representing days of the week.
  */
-enum class Weekday(override val value: Int) : IntValue {
-    MONDAY(1),
-    TUESDAY(2),
-    WEDNESDAY(3),
-    THURSDAY(4),
-    FRIDAY(5),
-    SATURDAY(6),
-    SUNDAY(7),
+enum class Weekday(
+    override val value: Int,
+    val calendarWeekday: Int,
+) : IntValue {
+    MONDAY(1, Calendar.MONDAY),
+    TUESDAY(2, Calendar.TUESDAY),
+    WEDNESDAY(3, Calendar.WEDNESDAY),
+    THURSDAY(4, Calendar.THURSDAY),
+    FRIDAY(5, Calendar.FRIDAY),
+    SATURDAY(6, Calendar.SATURDAY),
+    SUNDAY(7, Calendar.SUNDAY),
     ;
 
     companion object {
