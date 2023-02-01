ced670dabe263df1700dabd6291a5e919e147678
Yevhen Stepiuk <yevhen.stepiuk@vimeo.com>
2023-02-01 13:08:46 +0200

    CXAN-71 Migrate TimeAdapter.fromJson() to Regex/Calendar

--- models-core/src/main/java/com/vimeo/networking2/adapters/TimeAdapter.kt
+++ models-core/src/main/java/com/vimeo/networking2/adapters/TimeAdapter.kt
@@ -3,17 +3,17 @@ package com.vimeo.networking2.adapters
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
+import java.util.GregorianCalendar
 import java.util.TimeZone
 
 @Suppress("SimpleDateFormat")
 class TimeAdapter {
-    private val outputFormat = SimpleDateFormat(API_TIME_FORMAT)
-    private val appenderFormat = SimpleDateFormat(ALIGNMENT_DATE_FORMAT)
-    private val parserFormat = SimpleDateFormat(createParserTimestamp(ALIGNMENT_DATE_FORMAT, API_TIME_FORMAT))
-
-    init {
-        sequenceOf(outputFormat, appenderFormat, parserFormat).forEach { it.timeZone = TimeZone.getTimeZone("UTC") }
+    private val timeZone = TimeZone.getTimeZone("UTC")
+    private val timeRegex = Regex("^(\\d{2}):(\\d{2}):(\\d{2})Z$")
+    private val outputFormat = SimpleDateFormat("HH:mm:ss'Z'").apply {
+        timeZone = this@TimeAdapter.timeZone
     }
 
     @ToJson
@@ -23,16 +23,14 @@ class TimeAdapter {
     @Time
     fun fromJson(value: String?): Date? {
         if (value == null) return null
-        // This is needed to align time, since it could vary depending on date and time zone
-        // 01-01-1970T22:00:00Z transforms into 1:00 AM GMT, however should be 12:00 AM.
-        val aligned = createParserTimestamp(appenderFormat.format(Date()), value)
-        return parserFormat.parse(aligned)
-    }
-
-    companion object {
-        private const val API_TIME_FORMAT = "HH:mm:ss'Z'"
-        private const val ALIGNMENT_DATE_FORMAT = "yyyy-MM-dd"
-
-        fun createParserTimestamp(date: String, time: String) = "$date $time"
+        // Can't use outputFormat, since time could vary depending on date and time zone.
+        // E.g. 01-01-1970T22:00:00Z transforms into 1:00 AM GMT, however should be 12:00 AM.
+        val (hour, minute, second) = timeRegex.matchEntire(value)?.destructured ?: return null
+        return GregorianCalendar(timeZone).apply {
+            set(Calendar.HOUR_OF_DAY, hour.toInt())
+            set(Calendar.MINUTE, minute.toInt())
+            set(Calendar.SECOND, second.toInt())
+            set(Calendar.MILLISECOND, 0)
+        }.time
     }
 }
